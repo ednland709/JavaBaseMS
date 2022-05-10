60d0741f9c44fc543f51eb5b4ae23fe4eb01ee2d
ednland709 <ednland@hotmail.om>
2022-05-09 22:21:04 -0500

    se arregla gateay config

--- JavaBaseMV/Person/src/main/java/com/edwuin/feignclients/IGeographyClient.java
+++ JavaBaseMV/Person/src/main/java/com/edwuin/feignclients/IGeographyClient.java
@@ -1,16 +1,12 @@
 package com.edwuin.feignclients;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.edwuin.model.City;
 
-@FeignClient(name="geography-service")
-@RequestMapping("geography")
-@Service
+@FeignClient(name="geography-service", path="/geography")
 public interface IGeographyClient {
 	@GetMapping(value="/{id}")
 	public City GetCityById(@RequestParam (name="id", required = true) Long id);
